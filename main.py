cb21b247b98abbabda158328517a45c6e905044a
Matin Biabanpour <74891205+matin-biabanpour@users.noreply.github.com>
2022-02-05 22:10:15 +0330

    Added Functionality To Buttons
    Functions added for Number and Delete buttons to work

--- main.py
+++ main.py
@@ -3,11 +3,34 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from calculator import Ui_Window
 
+
 class calculator(QMainWindow, Ui_Window):
 	def __init__(self, *args, obj=None, **kwargs):
 		super().__init__()
 		self.setupUi(self)
+
+		# Adding Functionality For Each Num Button
+		self.numZero.clicked.connect(lambda: self.addToInput(0))
+		self.numOne.clicked.connect(lambda: self.addToInput(1))
+		self.numTwo.clicked.connect(lambda: self.addToInput(2))
+		self.numThree.clicked.connect(lambda: self.addToInput(3))
+		self.numFour.clicked.connect(lambda: self.addToInput(4))
+		self.numFive.clicked.connect(lambda: self.addToInput(5))
+		self.numSix.clicked.connect(lambda: self.addToInput(6))
+		self.numSeven.clicked.connect(lambda: self.addToInput(7))
+		self.numEight.clicked.connect(lambda: self.addToInput(8))
+		self.numNine.clicked.connect(lambda: self.addToInput(9))
+
+		# Add Funcionality To Delete Button
+		self.delBtn.clicked.connect(self.delFromLast)
+
 		self.show()
+
+	def addToInput(self, num):
+		self.inputField.setText(self.inputField.text() + str(num))
+
+	def delFromLast(self):
+		self.inputField.setText(self.inputField.text()[0:-1])
 
 if __name__ == '__main__':
 	app = QApplication([])
